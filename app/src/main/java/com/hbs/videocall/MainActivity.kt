3cd7e71c28cd77aeb256fbd14150057eaabd0a7d
nguyenkhacthe <the.nguyen@codegym.vn>
2021-08-26 18:00:51 +0700

    add Call activity screen

---
+++ app/src/main/java/com/hbs/videocall/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.hbs.videocall
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+    val requestCode = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(!isPermissionGranted()) {
+            askPermissions()
+        }
+
+        Firebase.initialize(this)
+
+        loginBtn.setOnClickListener {
+            val username = usernameEdit.text.toString()
+            val intent =  Intent(this, CallActivity::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        permissions.forEach {
+            if(ActivityCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED)
+                return false
+        }
+        return true
+    }
+
+    private fun askPermissions() {
+        ActivityCompat.requestPermissions(this, permissions, requestCode)
+    }
+}
