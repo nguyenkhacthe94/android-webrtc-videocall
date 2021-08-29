2a0cff653de53945a8d01302cf8c229204508df3
nguyenkhacthe <the.nguyen@codegym.vn>
2021-08-29 17:47:37 +0700

    remove firebase

--- app/src/main/assets/call.js
+++ app/src/main/assets/call.js
@@ -20,7 +20,7 @@ function init(userId) {
     });
 
     peer.on('open', () => {
-
+        Android.onPeerConnected()
     })
 
     listen();
--- app/src/main/java/com/hbs/videocall/CallActivity.kt
+++ app/src/main/java/com/hbs/videocall/CallActivity.kt
@@ -2,17 +2,24 @@ package com.hbs.videocall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.PermissionRequest
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_call.*
+import java.util.*
 
 class CallActivity : AppCompatActivity() {
     var username = ""
-    var friendUsername = ""
+    var friendsUsername = ""
+
     var isPeerConnected = false
 
     var firebaseRef = Firebase.database.getReference("users")
@@ -27,23 +34,40 @@ class CallActivity : AppCompatActivity() {
         username = intent.getStringExtra("username")!!
 
         callBtn.setOnClickListener {
-            //TODO
+            friendsUsername = friendNameEdit.text.toString()
+            sendCallRequest()
         }
 
         toggleAudioBtn.setOnClickListener {
-            //TODO
+            isAudio = !isAudio
+            callJavascriptFunction("javascript:toggleAudio(\"${isAudio}\")")
+            toggleAudioBtn.setImageResource(if (isAudio) R.drawable.ic_baseline_mic_24 else R.drawable.ic_baseline_mic_off_24)
         }
 
         toggleVideoBtn.setOnClickListener {
-            //TODO
+            isVideo = !isVideo
+            callJavascriptFunction("javascript:toggleVideo(\"${isVideo}\")")
+            toggleVideoBtn.setImageResource(if (isVideo) R.drawable.ic_baseline_videocam_24 else R.drawable.ic_baseline_videocam_off_24)
         }
 
         setupWebView()
-
     }
 
+    private fun sendCallRequest() {
+        if (!isPeerConnected) {
+            Toast.makeText(this, "You're not connected. Check your internet", Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+
+        switchToControls()
+        callJavascriptFunction("javascript:startCall(\"${friendsUsername}\")")
+    }
+
+
     private fun setupWebView() {
-        webView.webChromeClient = object: WebChromeClient() {
+
+        webView.webChromeClient = object : WebChromeClient() {
             override fun onPermissionRequest(request: PermissionRequest?) {
                 request?.grant(request.resources)
             }
@@ -57,18 +81,77 @@ class CallActivity : AppCompatActivity() {
     }
 
     private fun loadVideoCall() {
-        var filePath = "file:android_asset/call.html"
+        val filePath = "file:android_asset/call.html"
         webView.loadUrl(filePath)
+
         webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
+                initializePeer()
             }
         }
     }
+
+//    var uniqueId = ""
+
+    private fun initializePeer() {
+
+
+
+        callJavascriptFunction("javascript:init(\"${username}\")")
+//        firebaseRef.child(username).child("incoming").addValueEventListener(object: ValueEventListener {
+//            override fun onCancelled(error: DatabaseError) {}
+//
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                onCallRequest(snapshot.value as? String)
+//            }
+//
+//        })
+        onCallRequest(username)
+
+    }
+
+    private fun onCallRequest(caller: String?) {
+        if (caller == null) return
+
+        callLayout.visibility = View.VISIBLE
+        incomingCallTxt.text = "$caller is calling..."
+
+        acceptBtn.setOnClickListener {
+//            firebaseRef.child(username).child("connId").setValue(uniqueId)
+//            firebaseRef.child(username).child("isAvailable").setValue(true)
+
+            callLayout.visibility = View.GONE
+            switchToControls()
+        }
+
+        rejectBtn.setOnClickListener {
+//            firebaseRef.child(username).child("incoming").setValue(null)
+            callLayout.visibility = View.GONE
+        }
+
+    }
+
+    private fun switchToControls() {
+        inputLayout.visibility = View.GONE
+        callControlLayout.visibility = View.VISIBLE
+    }
+
+    private fun callJavascriptFunction(functionString: String) {
+        webView.post { webView.evaluateJavascript(functionString, null) }
+    }
+
 
     fun onPeerConnected() {
         isPeerConnected = true
     }
 
+    override fun onBackPressed() {
+        finish()
+    }
 
+    override fun onDestroy() {
+        firebaseRef.child(username).setValue(null)
+        webView.loadUrl("about:blank")
+        super.onDestroy()
+    }
 }
